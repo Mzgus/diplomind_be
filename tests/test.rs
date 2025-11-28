bc7e7e46c774c2a92da3ad6240137e9cd59e0aed
Mzgus <ilanetgus77@gmail.com>
2025-11-28 12:02:05 +0100

    initial setup

---
+++ tests/test.rs
@@ -0,0 +1,10 @@
+use diplomind::routes;
+use poem::test::TestClient;
+
+#[tokio::test]
+async fn base_test() {
+    let cli = TestClient::new(routes());
+    let resp = cli.get("/").send().await;
+    resp.assert_status_is_ok();
+    resp.assert_text("hello").await;
+}
