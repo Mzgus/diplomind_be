c205115a2fcf66b8664a2cd98cb93555dab6efc1
Mzgus <ilanetgus77@gmail.com>
2025-12-20 14:03:44 +0100

    clear cookies added + format code

--- src/handlers/auth.rs
+++ src/handlers/auth.rs
@@ -1,15 +1,14 @@
-use crate::{models, errors, db, services::*};
+use crate::{db, errors, models, services::*};
 use poem::web::cookie::CookieJar;
 use poem::web::{Data, Json};
 use sqlx::*;
 
-
 #[poem::handler]
 pub async fn login(
-    Data(executor): Data<&Pool<Postgres>>, 
-    Data(token_manager): Data<&TokenManager>, 
-    Json(user_auth): Json<models::UserAuth>, 
-    cookie_jar: &CookieJar
+    Data(executor): Data<&Pool<Postgres>>,
+    Data(token_manager): Data<&TokenManager>,
+    Json(user_auth): Json<models::UserAuth>,
+    cookie_jar: &CookieJar,
 ) -> Result<Json<models::AccessToken>, errors::MyError> {
     let user_info: models::User = match db::auth::login(executor, &user_auth.email).await {
         Ok(res) => res,
@@ -17,69 +16,85 @@ pub async fn login(
     };
     let user_auth_id: i32 = match db::auth::get_auth_id_by_email(executor, &user_auth.email).await {
         Ok(res) => res,
-        Err(err) => return Err(err)
+        Err(err) => return Err(err),
     };
     // Comme le mot de passe sera haché dans la BDD il faut aussi hacher le mdp donner pour pouvoir les comparer
     if user_info.user_pwd != user_auth.pwd {
-        return Err(errors::MyError::InvalidInput { input_type: "password" });
+        return Err(errors::MyError::InvalidInput {
+            input_type: "password",
+        });
     }
-    
-    let (access_token, refresh_token) = match token_manager.generate_token_pair(token_manager.clone(), user_info) {
-        Ok(res) => res,
-        Err(err) => return Err(err)
-    };
 
-    match token_manager.manage_token(executor, cookie_jar, &refresh_token, user_auth_id).await {
+    let (access_token, refresh_token) =
+        match token_manager.generate_token_pair(token_manager.clone(), user_info) {
+            Ok(res) => res,
+            Err(err) => return Err(err),
+        };
+
+    match token_manager
+        .manage_token(executor, cookie_jar, &refresh_token, user_auth_id)
+        .await
+    {
         Err(err) => return Err(err),
-        _ => {},
+        _ => {}
     }
 
-    Ok(Json(crate::AccessToken { token: access_token }))   
+    Ok(Json(crate::AccessToken {
+        token: access_token,
+    }))
 }
 
 #[poem::handler]
 pub async fn refresh_tokens(
-    Data(executor): Data<&Pool<Postgres>>, 
-    Data(token_manager): Data<&TokenManager>, 
-    cookie_jar: &CookieJar
+    Data(executor): Data<&Pool<Postgres>>,
+    Data(token_manager): Data<&TokenManager>,
+    cookie_jar: &CookieJar,
 ) -> Result<Json<models::AccessToken>, errors::MyError> {
     let token = match token_manager.get_cookie_value(cookie_jar) {
         Ok(res) => res,
-        Err(err) => return Err(err)
+        Err(err) => return Err(err),
     };
-    
-    let refresh_token: models::RefreshToken = match db::auth::get_refresh_token(executor, token.clone()).await {
-        Ok(res) => res,
-        Err(err) => return Err(err)
-    };
+
+    let refresh_token: models::RefreshToken =
+        match db::auth::get_refresh_token(executor, token.clone()).await {
+            Ok(res) => res,
+            Err(err) => return Err(err),
+        };
 
     if !token_manager.verify_token_validity(refresh_token.expiration_date) {
         return Err(errors::MyError::TokenExpired);
     }
 
-    let user_info: models::User = match db::auth::get_user_info_by_token(executor, token.clone()).await {
-        Ok(res) => res,
+    let user_info: models::User =
+        match db::auth::get_user_info_by_token(executor, token.clone()).await {
+            Ok(res) => res,
+            Err(err) => return Err(err),
+        };
+
+    let user_auth_id: i32 =
+        match db::auth::get_auth_id_by_email(executor, &user_info.user_email).await {
+            Ok(res) => res,
+            Err(err) => return Err(err),
+        };
+
+    let (access_token, refresh_token) =
+        match token_manager.generate_token_pair(token_manager.clone(), user_info) {
+            Ok(res) => res,
+            Err(err) => return Err(err),
+        };
+
+    match token_manager
+        .manage_token(executor, cookie_jar, &refresh_token, user_auth_id)
+        .await
+    {
         Err(err) => return Err(err),
-    };
-
-    let user_auth_id: i32 = match db::auth::get_auth_id_by_email(executor, &user_info.user_email).await {
-        Ok(res) => res,
-        Err(err) => return Err(err)
-    };
-
-    let (access_token, refresh_token) = match token_manager.generate_token_pair(token_manager.clone(), user_info) {
-        Ok(res) => res,
-        Err(err) => return Err(err)
-    };
-
-    match token_manager.manage_token(executor, cookie_jar, &refresh_token, user_auth_id).await {
-        Err(err) => return Err(err),
-        _ => {},
+        _ => {}
     }
-        
-    Ok(Json(crate::AccessToken { token: access_token })) 
-}
 
+    Ok(Json(crate::AccessToken {
+        token: access_token,
+    }))
+}
 
 //logout
 #[poem::handler]
@@ -87,7 +102,6 @@ pub async fn logout(
     Data(token_manager): Data<&TokenManager>,
     cookie_jar: &CookieJar,
 ) -> Result<(), errors::MyError> {
-    // cookie_jar.remove(token_manager.cookie_name.clone());
-    // token_manager.clear_cookie(cookie_jar);
+    token_manager.clear_cookie(cookie_jar);
     Ok(())
 }
--- src/services/auth.rs
+++ src/services/auth.rs
@@ -1,15 +1,14 @@
-use poem::web::cookie::CookieJar;
+use crate::MyError;
+use crate::db;
+use crate::{errors, models};
 use base64::prelude::*;
-use jsonwebtoken::*;
-use sqlx::*;
-use poem::web::cookie;
 use chrono::DateTime;
 use chrono::Duration;
 use chrono::prelude::*;
-use crate::db;
-use crate::{models, errors};
-use crate::MyError;
-
+use jsonwebtoken::*;
+use poem::web::cookie;
+use poem::web::cookie::CookieJar;
+use sqlx::*;
 
 #[derive(Clone)]
 pub struct TokenManager {
@@ -18,10 +17,11 @@ pub struct TokenManager {
 }
 
 impl TokenManager {
-    pub fn new(jwt_secret: String, cookie_name: String) -> TokenManager
-    {
-     TokenManager
-     { jwt_secret, cookie_name }
+    pub fn new(jwt_secret: String, cookie_name: String) -> TokenManager {
+        TokenManager {
+            jwt_secret,
+            cookie_name,
+        }
     }
 
     pub fn generate_access_token(
@@ -29,14 +29,16 @@ impl TokenManager {
         claims: models::JWTClaims,
     ) -> Result<String, errors::MyError> {
         let encoded_token = match encode(
-            &Header::default(), 
-            &claims,    
-            &EncodingKey::from_secret(self.jwt_secret.as_ref())
+            &Header::default(),
+            &claims,
+            &EncodingKey::from_secret(self.jwt_secret.as_ref()),
         ) {
             Ok(res) => res,
             Err(_) => {
-                return Err(errors::MyError::GenerationFailed { entity: "acces token" });
-            }    
+                return Err(errors::MyError::GenerationFailed {
+                    entity: "acces token",
+                });
+            }
         };
         Ok(encoded_token)
     }
@@ -47,19 +49,21 @@ impl TokenManager {
         getrandom::fill(&mut buf)?;
         Ok(buf)
     }
-    
-    pub fn generate_expiration_date(&self, duration: chrono::Duration) -> DateTime<Utc> { 
+
+    pub fn generate_expiration_date(&self, duration: chrono::Duration) -> DateTime<Utc> {
         let mut utc: DateTime<Utc> = Utc::now();
-        let validity_duration = duration; 
+        let validity_duration = duration;
         utc += validity_duration;
-        return utc
+        return utc;
     }
 
     pub fn generate_refresh_token() -> Result<String, errors::MyError> {
         let token_bytes: [u8; 32] = match TokenManager::get_random_u128() {
             Ok(res) => res,
             Err(_) => {
-                return Err(errors::MyError::GenerationFailed { entity: "refresh token" });
+                return Err(errors::MyError::GenerationFailed {
+                    entity: "refresh token",
+                });
             }
         };
         let token: String = BASE64_STANDARD.encode(token_bytes);
@@ -83,55 +87,69 @@ impl TokenManager {
 
         // let expiration_date = self.generate_expiration_date(chrono::Duration::weeks(1));
         cookie.set_expires(expiration_date);
-    
+
         cookie
     }
 
     pub fn get_cookie_value(&self, cookie_jar: &CookieJar) -> Result<String, errors::MyError> {
         let cookie_name = &self.cookie_name;
         let refresh_token = match cookie_jar.get(&cookie_name) {
-            Some(res) => { 
-                match res.value::<String>() {
-                    Ok(res) => res,
-                    Err(err) => return Err(MyError::CookieError(err))
-                }
+            Some(res) => match res.value::<String>() {
+                Ok(res) => res,
+                Err(err) => return Err(MyError::CookieError(err)),
             },
-            None => "".to_string()
+            None => "".to_string(),
         };
         Ok(refresh_token)
     }
 
     pub fn clear_cookie(&self, cookie_jar: &CookieJar) {
-        // let mut cookie = cookie::Cookie::named(self.cookie_name.clone());
-        // cookie.set_path("/api/refresh_tokens");
-        // cookie.set_http_only(true);
-        // cookie.set_secure(true);
-        // cookie.set_same_site(poem::web::cookie::SameSite::Lax);
-        // cookie_jar.remove(self.cookie_name.clone());
+        let mut cookie = cookie::Cookie::named(self.cookie_name.clone());
+        cookie.set_path("/api/refresh_tokens");
+        cookie.set_http_only(true);
+        cookie.set_secure(true);
+        cookie.set_same_site(poem::web::cookie::SameSite::Lax);
+        cookie.set_expires(Utc::now() - chrono::Duration::days(1)); // Expire immediately
+        cookie_jar.add(cookie); // Overwrite the existing cookie with the expired one
     }
 
-    pub fn generate_token_pair(&self, token_manager: TokenManager, user_info: models::auth::User) -> Result<(String, String), errors::MyError> {
-        let exp = token_manager.generate_expiration_date(chrono::Duration::minutes(5)).timestamp() as usize;
-        let claims = models::JWTClaims{user: user_info, exp};
-        
+    pub fn generate_token_pair(
+        &self,
+        token_manager: TokenManager,
+        user_info: models::auth::User,
+    ) -> Result<(String, String), errors::MyError> {
+        let exp = token_manager
+            .generate_expiration_date(chrono::Duration::minutes(5))
+            .timestamp() as usize;
+        let claims = models::JWTClaims {
+            user: user_info,
+            exp,
+        };
+
         let access_token: String = match TokenManager::generate_access_token(self, claims) {
             Ok(res) => res,
-            Err(err) => return Err(err)
+            Err(err) => return Err(err),
         };
         let refresh_token: String = match TokenManager::generate_refresh_token() {
             Ok(res) => res,
-            Err(err) => return Err(err)
+            Err(err) => return Err(err),
         };
         Ok((access_token, refresh_token))
     }
 
-    pub async fn manage_token<'e>(&self, executor: &Pool<Postgres>, cookie_jar: &CookieJar, refresh_token: &String, user_auth_id: i32) -> Result<(), MyError > {
+    pub async fn manage_token<'e>(
+        &self,
+        executor: &Pool<Postgres>,
+        cookie_jar: &CookieJar,
+        refresh_token: &String,
+        user_auth_id: i32,
+    ) -> Result<(), MyError> {
         // enregistrer le refresh d ans la db
         let exp: DateTime<Utc> = self.generate_expiration_date(chrono::Duration::weeks(2));
 
         match db::create_refresh_token(executor, user_auth_id, refresh_token.clone(), exp).await {
-            Ok(_) => {},
-            Err(err) => return Err(err)
+            Ok(_) => {}
+            Err(err) => return Err(err),
         };
         let cookie = self.create_cookie(refresh_token.clone(), exp);
         cookie_jar.add(cookie);
